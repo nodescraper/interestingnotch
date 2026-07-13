1532b1a5b25cfe5401ea26dac7b3cbb472c4a671
Balazs Csutar <nodescraper.dovco@passmail.com>
2026-07-13 09:30:44 +0200

    M1.T1.9a: wire widget page into notch

--- boringNotch/components/Tabs/TabSelectionView.swift
+++ boringNotch/components/Tabs/TabSelectionView.swift
@@ -16,7 +16,8 @@ struct TabModel: Identifiable {
 
 let tabs = [
     TabModel(label: "Home", icon: "house.fill", view: .home),
-    TabModel(label: "Shelf", icon: "tray.fill", view: .shelf)
+    TabModel(label: "Shelf", icon: "tray.fill", view: .shelf),
+    TabModel(label: "Widgets", icon: "square.grid.2x2", view: .widgets)
 ]
 
 struct TabSelectionView: View {
